6ca2e37fd425772bcb122fef2e0f56ccd3c9aa48
aledx18 <alediiaz18@gmail.com>
2025-04-03 16:08:12 -0300

    Refactor GalleryGrid to use Card component and add search functionality

--- components/gallery/gallery-grid.tsx
+++ components/gallery/gallery-grid.tsx
@@ -3,6 +3,11 @@
 import { CldImage } from 'next-cloudinary'
 import { ImageGrid } from './image-grid'
 import { SearchResult } from './list'
+import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card'
+import { Heading } from '@/components/ui/heading'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { SearchIcon } from 'lucide-react'
 
 export default function GalleryGrid({
   images
@@ -10,21 +15,42 @@ export default function GalleryGrid({
   images: (SearchResult & { base64?: string })[]
 }) {
   return (
-    <ImageGrid
-      images={images}
-      getImage={(imageData: SearchResult) => {
-        return (
-          <CldImage
-            key={imageData.public_id}
-            src={imageData.public_id}
-            quality={20}
-            width='400'
-            height='300'
-            className='rounded-lg'
-            alt={`Image ${imageData.public_id}`}
+    <Card>
+      <CardHeader>
+        <div className='flex justify-between items-center'>
+          <Heading
+            title='Ghiblol'
+            description='A collection of beautiful illustrations made by Ghiblo.'
           />
-        )
-      }}
-    />
+          <div className='flex gap-2 items-center'>
+            <Input placeholder='Image...' />
+            <Button>
+              <SearchIcon />
+              <span className='sr-only'>Search</span>
+              Search
+            </Button>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ImageGrid
+          images={images}
+          getImage={(imageData: SearchResult) => {
+            return (
+              <CldImage
+                key={imageData.public_id}
+                src={imageData.public_id}
+                quality={20}
+                width='400'
+                height='300'
+                className='rounded-lg'
+                alt={`Image ${imageData.public_id}`}
+              />
+            )
+          }}
+        />
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
   )
 }
